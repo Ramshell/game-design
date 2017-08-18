ffb7816ac7880d92e735d756daebad15433e3fcd
Ramshell <ramshellcinox@gmail.com>
2017-08-18 10:44:07 -0300

    Add minion title and setting up the width and height

--- minion/desktop/src/ar/unq/minion/desktop/DesktopLauncher.java
+++ minion/desktop/src/ar/unq/minion/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import ar.unq.minion.MinionGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Minion 1";
+		config.width = 480;
+		config.height = 800;
 		new LwjglApplication(new MinionGame(), config);
 	}
 }
