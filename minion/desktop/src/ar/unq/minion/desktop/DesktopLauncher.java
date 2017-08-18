a48d537380d91922ccb9199f7474416f5c37831f
Ramshell <ramshellcinox@gmail.com>
2017-08-18 10:33:00 -0300

    Minion kickoff

---
+++ minion/desktop/src/ar/unq/minion/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ar.unq.minion.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ar.unq.minion.MinionGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MinionGame(), config);
+	}
+}
